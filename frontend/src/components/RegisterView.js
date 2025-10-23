e8a8c7fd9c7adf2cfdd88d41c17f9423113e9386
lucasl <lucas.lemaitre@ctg.com>
2025-10-23 14:40:22 +0200

    register

--- frontend/src/components/RegisterView.js
+++ frontend/src/components/RegisterView.js
@@ -33,7 +33,7 @@ export default function RegisterView({ onRegister, onSwitchToLogin }) {
     e.preventDefault();
 
     try {
-      const res = await fetch('https://kinekaty.onrender.com/auth/register', {
+      const res = await fetch('https://kinekaty.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
