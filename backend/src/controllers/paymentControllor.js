35d1298cb11d182ea3dbe51f8d5edbb2839b4e50
lucasl <lucas.lemaitre@ctg.com>
2025-10-28 14:52:51 +0100

    doorsturen userId

--- backend/src/controllers/paymentControllor.js
+++ backend/src/controllers/paymentControllor.js
@@ -7,8 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const {addTokensToUserHelper} = require('./tokenController');
 
 const createSession = async (req, res) => {
-  const { userId } = req.user.id
-  const { amount, tokens } = req.body;
+  const { amount, tokens, userId } = req.body;
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
